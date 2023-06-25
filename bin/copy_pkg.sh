333d65773ab72e212977fdfe9c0ffe29f0415c07
Lokawn <39772448+Lokawn@users.noreply.github.com>
2023-06-26 02:01:40 +0530

    Remove one more bash errors

--- bin/copy_pkg.sh
+++ bin/copy_pkg.sh
@@ -4,16 +4,19 @@ mkdir -vp "$GITHUB_WORKSPACE"/{pkgdir,logdir} &> $DEBUG_OFF
 copy_pkg_files() {
     echo "::group::Copying package files from previous remote to PKGDIR."
 
-    for i in $GITHUB_WORKSPACE/repo/x86_64/*.zst
+    for i in "$GITHUB_WORKSPACE/repo/x86_64"/*.zst
     do
-        [[ ! -e "${i}.sig" ]] && rm -rvf "$i" && continue
+        local app_name package_file
+        package_file=${i##*repo*/}
+        app_name=${package_file%%-[0-9]*}
+        [[ ! -e "${i}.sig" ]] && rm -rvf "$i" &> $DEBUG_OFF && echo -e "\e[1;33mSignature for ${app_name} not found, package file removed.\e[0m" && continue
         cp -v "$i" "${GITHUB_WORKSPACE}/pkgdir/" &> $DEBUG_OFF
         if [[ -n "$ENABLE_DEBUG" && "$ENABLE_DEBUG" = false \
           || -z "$ENABLE_DEBUG" ]]; then
-            echo -e "\e[0;34mCopied ${i##*repo*/} to ${GITHUB_WORKSPACE}/pkgdir.\e[0m"
+            echo -e "\e[0;34mCopied ${package_file} to '${GITHUB_WORKSPACE}/pkgdir'.\e[0m"
         fi
     done
-    echo "Options +Indexes" > "$GITHUB_WORKSPACE/repo/.htaccess" &> $DEBUG_OFF
+    echo "Options +Indexes" | tee "$GITHUB_WORKSPACE/repo/.htaccess" &> $DEBUG_OFF
     echo "::endgroup::"
 }
 
@@ -22,8 +25,8 @@ seg_pkg_files() {
     echo "::group::Delete unneeded files from PKGDIR."
 
     while read -r pkg && [[ -n $pkg ]] || [[ -n $pkg ]]; do
-    if compgen -G "$GITHUB_WORKSPACE/pkgdir/${pkg}"[0-9]*.zst &> $DEBUG_OFF; then
-        find "$GITHUB_WORKSPACE/pkgdir/" -name "${pkg}*" -delete
+    if compgen -G "$GITHUB_WORKSPACE/pkgdir/${pkg}"-[0-9]*.zst &> $DEBUG_OFF; then
+        find "$GITHUB_WORKSPACE/pkgdir/" -name "${pkg}-[0-9]*" -delete
         echo -e "\e[1;33m${pkg} to be updated.\e[0m"
     else
         echo -e "\e[0;34m${pkg} is new.\e[0m"
