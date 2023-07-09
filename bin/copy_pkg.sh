9d6f8ebcbb0abc9dfe4a2e951fc3c37634a5d025
Lokawn <39772448+Lokawn@users.noreply.github.com>
2023-07-09 17:20:59 +0530

    retry 1

--- bin/copy_pkg.sh
+++ bin/copy_pkg.sh
@@ -1,15 +1,21 @@
 mkdir -vp "$GITHUB_WORKSPACE"/{pkgdir,logdir} &> $DEBUG_OFF
 
-# Copy package files from previous remote to PKGDIR.
+# Copy package files from sync directory to PKGDIR.
 copy_pkg_files() {
-    echo "::group::Copying package files from previous remote to PKGDIR."
+    echo "::group::Copying package files from sync directory to PKGDIR."
 
     for i in "$GITHUB_WORKSPACE/repo/x86_64"/*.zst
     do
         local app_name package_file
         package_file=${i##*repo*/}
         app_name=${package_file%%-[0-9]*}
-        [[ ! -e "${i}.sig" ]] && rm -rvf "$i" &> $DEBUG_OFF && echo -e "\e[1;33mSignature for ${app_name} not found, package file removed.\e[0m" && if [[ -d "${GITHUB_WORKSPACE}/pkgs/${app_name}" ]]; then echo ${app_name} | tee -a "${GITHUB_WORKSPACE}/pkglist"; fi && continue
+
+        [[ ! -e "${i}.sig" ]] && rm -rvf "$i" &> $DEBUG_OFF && \
+        echo -e "\e[1;33mSignature for ${app_name} not found, package file removed.\e[0m" && \
+        [[ -d "${GITHUB_WORKSPACE}/pkgs/${app_name}" ]] && \
+        echo ${app_name} | tee -a "${GITHUB_WORKSPACE}/pkglist" && \
+        continue
+
         cp -v "$i"{,.sig} "${GITHUB_WORKSPACE}/pkgdir/" &> $DEBUG_OFF
         if [[ -n "$ENABLE_DEBUG" && "$ENABLE_DEBUG" = false \
           || -z "$ENABLE_DEBUG" ]]; then
@@ -24,8 +30,10 @@ copy_pkg_files() {
 seg_pkg_files() {
     echo "::group::Delete unneeded files from PKGDIR."
 
+    cd "$GITHUB_WORKSPACE/pkgdir/"
+
     while read -r pkg && [[ -n $pkg ]] || [[ -n $pkg ]]; do
-    if compgen -G "$GITHUB_WORKSPACE/pkgdir/${pkg}"-[0-9]*.zst &> $DEBUG_OFF; then
+    if compgen -G "${pkg}-[0-9]*.zst" &> $DEBUG_OFF; then
         find "$GITHUB_WORKSPACE/pkgdir/" -name "${pkg}-[0-9]*" -delete
         echo -e "\e[1;33m${pkg} to be updated.\e[0m"
     else
