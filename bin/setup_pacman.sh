01dc1cc312b787cd0e0f0a4e430df5fe65fb366b
Lokawn <39772448+Lokawn@users.noreply.github.com>
2023-09-16 19:27:23 +0530

    Add error handling

--- bin/setup_pacman.sh
+++ bin/setup_pacman.sh
@@ -1,10 +1,15 @@
 # Setup pacman, from
 # https://gitlab.archlinux.org/archlinux/archlinux-docker/-/blob/master/README.md
+
 if [[ -n "$ENABLE_DEBUG" && "$ENABLE_DEBUG" = true ]]; then
     echo -e "::group::${GREEN_COLOR}Initializing pacman.${UNSET_COLOR}"
 fi
+
 pacman-key --init --verbose &> $DEBUG_OFF
+[[ $(echo $?) ]] || (echo -e "${RED_COLOR}${BOLD_TEXT}Pacman-key failed to initialize.${UNSET_COLOR}" && exit 1)
+
 pacman-key --populate --verbose archlinux &> $DEBUG_OFF
+[[ `echo $?` ]] || (echo -e "${RED_COLOR}${BOLD_TEXT}Pacman-key failed to populate.${UNSET_COLOR}" && exit 1)
 
 if [[ -n "$ENABLE_DEBUG" && "$ENABLE_DEBUG" = true ]]; then
     echo "::endgroup::"
@@ -24,9 +29,28 @@ then
     fi
 fi
 
-sed -i '/\[core\]/i \[repo\]\nServer\ =\ file:\/\/\/github\/workspace\/repo\/x86_64-old' /etc/pacman.conf
+# Add local repo to pacman.conf
+if sed -i '/\[core\]/i \[repo\]\nServer\ =\ file:\/\/\/github\/workspace\/repo\/x86_64-old' /etc/pacman.conf
+then
+    echo -e "${BLUE_COLOR}${BOLD_TEXT}Enabled local repository in 'pacman.conf'.${UNSET_COLOR}"
+else
+    echo -e "${RED_COLOR}${BOLD_TEXT}Failed to enable local repository, necessary for building packages - aborting.${UNSET_COLOR}" && exit 1
+fi
 
 # just to remove warning.
 for i in {1..5}
-do pacman -Sy &> $DEBUG_OFF && break || sleep 5
-done
+do
+#    if [[ $i -le 5 ]]
+#    then
+        if pacman -Sy &> $DEBUG_OFF
+        then
+            echo -e "${BLUE_COLOR}${BOLD_TEXT}Pacman database updated.${UNSET_COLOR}"
+        else
+            if [[ $i -le 4 ]]; then
+                (echo -e "${RED_COLOR}${BOLD_TEXT}Pacman database update failed - retrying.${UNSET_COLOR}" && exit 1)
+            elif [[ $i == 5 ]]; then
+                (echo -e "${RED_COLOR}${BOLD_TEXT}Pacman database update failed, 5 times - aborting.${UNSET_COLOR}" && exit 1)
+            fi || (exit 1)
+        fi && break || (exit 1)
+#    fi || (sleep 5; exit 1)
+done || exit 1
--- bin/syncweb.sh
+++ bin/syncweb.sh
@@ -4,7 +4,12 @@
 check_sfhost() {
     if ! grep -Fxq "$HOST" "/home/runner/.ssh/known_hosts"
     then
-        echo "$HOST" | tee -a "/home/runner/.ssh/known_hosts" &> /dev/null
+        if ! echo "$HOST" | tee -a "/home/runner/.ssh/known_hosts" &> /dev/null
+        then
+            echo -e "${RED_COLOR}${BOLD_TEXT}Failed to append REMOTE to '~/.ssh/known_hosts' - aborting.${UNSET_COLOR}"
+            exit 1
+        fi
+        echo -e "${BLUE_COLOR}${BOLD_TEXT}REMOTE appended to '~/.ssh/known_hosts' - proceeding.{UNSET_COLOR}"
     fi
 }
 
@@ -16,7 +21,13 @@ add_sfhost() {
         touch "/home/runner/.ssh/known_hosts"
         check_sfhost
     fi
-    chmod -v 0600 "/home/runner/.ssh/known_hosts" &> $DEBUG_OFF
+    if chmod -v 0600 "/home/runner/.ssh/known_hosts" &> $DEBUG_OFF
+    then
+        echo -e "${BLUE_COLOR}${BOLD_TEXT}Corrected permissions of '~/.ssh/known_hosts' - proceeding.{UNSET_COLOR}"
+    else
+        echo -e "${RED_COLOR}${BOLD_TEXT}Failed to set correct permissions of '~/.ssh/known_hosts' - aborting.${UNSET_COLOR}"
+        exit 1
+    fi
 }
 
 addkey() {
@@ -26,15 +37,23 @@ addkey() {
         if ! diff "/home/runner/.ssh/id_ed25519" "/home/runner/.ssh/id_ed25519.bak" &> $DEBUG_OFF
         then
             mv -v "/home/runner/.ssh/id_ed25519.bak" "/home/runner/.ssh/id_ed25519" &> $DEBUG_OFF
+            echo -e "${BLUE_COLOR}${BOLD_TEXT}Private SSH Key created - proceeding.{UNSET_COLOR}"
         fi
     else
         echo -e "$SSHKEY" | tee "/home/runner/.ssh/id_ed25519" &> /dev/null
+        echo -e "${BLUE_COLOR}${BOLD_TEXT}Private SSH Key created - proceeding.{UNSET_COLOR}"
     fi
     chmod -v 0600 "/home/runner/.ssh/id_ed25519" &> $DEBUG_OFF
 }
 
 rsyncfiles() {
-    rsync -a --delete -e "ssh -v -i /home/runner/.ssh/id_ed25519" "$1" "$2" &> $DEBUG_OFF
+    if rsync -a --delete -e "ssh -v -i /home/runner/.ssh/id_ed25519" "$1" "$2" &> $DEBUG_OFF
+    then
+        echo -e "${BLUE_COLOR}${BOLD_TEXT}Successfully synced files - proceeding.{UNSET_COLOR}"
+    else
+        echo -e "${RED_COLOR}${BOLD_TEXT}Failed to sync files - aborting.${UNSET_COLOR}"
+        return 1
+    fi || (exit 1)
 }
 
 add_sfhost
