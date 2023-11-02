155451990a6a11bef2fcc16de6a06a35e553753c
Lokawn <39772448+Lokawn@users.noreply.github.com>
2023-11-02 20:23:43 +0530

    Use RUNNER_DEBUG for enabling debug

--- bin/set_debug.sh
+++ bin/set_debug.sh
@@ -1,7 +1,7 @@
 #https://askubuntu.com/q/1168750/894787
 export DEBUG_OFF='/dev/null'
 
-if [[ -n "$ENABLE_DEBUG" && "$ENABLE_DEBUG" = true ]]; then
+if [[ -n "${RUNNER_DEBUG}" && "${RUNNER_DEBUG}" = 1 ]]; then
     set -x
     export DEBUG_OFF='/dev/stdout'
 fi
